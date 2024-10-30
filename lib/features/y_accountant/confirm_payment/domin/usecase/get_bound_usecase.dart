04e89abd3ab014d6585e0d22d63885a0e61a99b4
khaled al-amoudi <xxxkhald@gmail.com>
2024-10-30 19:40:37 +0300

    end domin layer

---
+++ lib/features/y_accountant/confirm_payment/domin/usecase/get_bound_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:test_order_life_cycle/core/errors/failure.dart';
+import 'package:test_order_life_cycle/core/use_cases/no_param_use_case.dart';
+import 'package:test_order_life_cycle/features/y_accountant/confirm_payment/domin/repo/y_accontant_repo.dart';
+
+class GetBoundUSeCase extends UseCaseWithNoParam {
+  final YAccontantRepo yAccontantRepo;
+
+  GetBoundUSeCase({required this.yAccontantRepo});
+
+  @override
+  Future<Either<Failure, dynamic>> execute() {
+  return  yAccontantRepo.getBound();
+  }
+  
+}
