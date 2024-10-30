4dceb9e72c80ca83347b026daa098254944f4900
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-31 01:09:06 +0300

    Update store_home_page_list_view_new.dart

--- lib/features/store/home/ui/widget/store_home_page_list_view_new.dart
+++ lib/features/store/home/ui/widget/store_home_page_list_view_new.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:test_order_life_cycle/features/store/home/ui/manger/cubit/all_order_cubit.dart';
+import 'package:test_order_life_cycle/features/store/home/ui/manger/cubit/all_order_state.dart';
 import 'package:test_order_life_cycle/features/store/home/ui/widget/order_details_button.dart';
 
 class StoreHomePageListViewNew extends StatelessWidget {
@@ -8,15 +13,41 @@ class StoreHomePageListViewNew extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: 10,
-        itemBuilder: (context, i) {
-          return const OrderDetailsButton(
-            bondNum: '654654',
-            date: '2024/10/2',
-            itemNum: '1',
+    return BlocBuilder<AllOrderCubit, AllOrderState>(
+      builder: (context, state) {
+        if (state is AllOrderSuccess) {
+          return ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: 10,
+              itemBuilder: (context, i) {
+                return OrderDetailsButton(
+                  bondNum: state.orders[i].orderNum,
+                  date: state.orders[i].orderDate.toString(),
+                  itemNum: state.orders[i].productNum.toString(),
+                );
+              });
+        } else if (state is AllOrderFailuer) {
+          return Text(state.errMessage);
+        } else {
+          return Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Container(
+                    color: Colors.white,
+                    height: 130.h,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                );
+              },
+            ),
           );
-        });
+        }
+      },
+    );
   }
 }
