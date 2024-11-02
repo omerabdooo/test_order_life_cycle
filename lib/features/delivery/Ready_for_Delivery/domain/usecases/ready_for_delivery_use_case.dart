d24d4e6d32ad9c9e5cf518f4aa2cdb8d5ca1064d
Bot200037 <salem.baqatyan775@gmail.com>
2024-11-02 13:14:48 +0300

    finish cubit

--- lib/features/delivery/Ready_for_Delivery/domain/usecases/ready_for_delivery_use_case.dart
+++ lib/features/delivery/Ready_for_Delivery/domain/usecases/ready_for_delivery_use_case.dart
@@ -4,10 +4,10 @@ import 'package:test_order_life_cycle/core/use_cases/no_param_use_case.dart';
 import 'package:test_order_life_cycle/features/delivery/Ready_for_Delivery/domain/entities/ready_for_delivery_entity.dart';
 import 'package:test_order_life_cycle/features/delivery/Ready_for_Delivery/domain/repo/parcel_delivery_repo.dart';
 
-class ReadyForDeliveryUseCase extends UseCaseWithNoParam {
+class GetAllReadyForDeliveryUseCase extends UseCaseWithNoParam {
   final ReadyForDeliveryRepo readyForDeliveryRepo;
 
-  ReadyForDeliveryUseCase(this.readyForDeliveryRepo);
+  GetAllReadyForDeliveryUseCase(this.readyForDeliveryRepo);
 
   @override
   Future<Either<Failure, List<ReadyForDeliveryEntity>?>> execute() {
---
+++ lib/features/delivery/Ready_for_Delivery/ui/widgets/build_list_view_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_order_life_cycle/core/styles/Colors.dart';
+import 'package:test_order_life_cycle/core/widgets/a_order.dart';
+
+/// Generic ListView Builder for view shipping address
+Widget buildListView<CubitType extends StateStreamable<StateType>, StateType, LoadingStateType, FailureStateType, SuccessStateType, T>({
+  required Widget Function(StateType state)? stateBuilder,
+  required int Function(StateType state)? itemCount,
+}) {
+  return Expanded(
+    child: BlocBuilder<CubitType, StateType>(
+      builder: (context, state) {
+        if (state is LoadingStateType) {
+          return const Center(child: CircularProgressIndicator(),);
+        } else if (state is FailureStateType) {
+          return Center(
+                    child: Text(
+                      (state as dynamic).errorMessage,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  );
+        // Handle success state
+        } else if (state is SuccessStateType) {
+          // Assuming SuccessStateType has a property called `customerAddress`
+          final deliveries = (state as dynamic).deliveries;
+
+          // Check if the address list is empty
+          if (deliveries.isEmpty) {
+            return const Center(
+              child: Text(
+                "لم يتم اضافة طلبات جاهزة للاستلام...",  // Message for empty list
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            );
+          }
+
+          // Display the list if it's not empty
+          return ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: deliveries.length, // Use the length of the list
+            physics: const AlwaysScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              final delivery = deliveries[index]; // Access the individual address
+
+              return KOrdersWidget(
+                      OrderNumber: delivery.idOrder,
+                      Phone: delivery.phoneNumber,
+                      Costmor: delivery.name,
+                      NumberParcels: delivery.idParcel,
+                      TotalNumber: delivery.totalParcels,
+                      Backcolor: AppColors.greyLight);
+            },
+          );
+        }
+
+        return stateBuilder?.call(state) ??
+            const SizedBox.shrink();
+      },
+    ),
+  );
+}
