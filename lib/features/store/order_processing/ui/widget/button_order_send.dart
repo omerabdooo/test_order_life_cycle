fca26de3df5ab47052b5db190859888b21e4cec6
qais25 <qaisabdullahbafayadh@gmail.com>
2024-11-12 18:22:47 +0300

    Update button_order_send.dart

--- lib/features/store/order_processing/ui/widget/button_order_send.dart
+++ lib/features/store/order_processing/ui/widget/button_order_send.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:test_order_life_cycle/core/functions/image_picker_function.dart';
 import 'package:test_order_life_cycle/core/setup_service_locator.dart';
 import 'package:test_order_life_cycle/core/widgets/custom_data_dialog_widget.dart';
@@ -14,67 +15,67 @@ class ButtonOrderSend extends StatelessWidget {
   const ButtonOrderSend({
     super.key,
     // this.testConroller,
-    this.dateConrollers,
-    this.numberConrollers,
-    this.mountConrollers,
+    // this.dateConrollers,
+    // this.numberConrollers,
+    // this.mountConrollers,
   });
   // final TextEditingController? testConroller;
-  final TextEditingController? dateConrollers;
-  final TextEditingController? numberConrollers;
-  final TextEditingController? mountConrollers;
+  // final TextEditingController? dateConrollers;
+  // final TextEditingController? numberConrollers;
+  // final TextEditingController? mountConrollers;
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: KCustomPrimaryButtonWidget(
-          buttonName: "ارسال الطلب",
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return CustomDataDialogWidgetShipping(
-                    headTitle: 'بيانات الشحن',
-                    firstTitle: 'التاريخ',
-                    secondTitle: 'رقم فاتورة الشحن',
-                    thierdTitle: 'شركة النقل',
-                    onPressedSure: () async {
-                      String mountText = mountConrollers!.text;
-                      try {
-                        await context.read<ShippingCubit>().fechOrderShipping(
-                              ids ?? [],
-                              numberConrollers!.text,
-                              images!,
-                              mountText,
+    return KCustomPrimaryButtonWidget(
+        height: 20.h,
+        width: 200.w,
+        buttonName: "ارسال الطلب",
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return CustomDataDialogWidgetShipping(
+                  headTitle: 'بيانات الشحن',
+                  firstTitle: 'التاريخ',
+                  secondTitle: 'رقم فاتورة الشحن',
+                  thierdTitle: 'شركة النقل',
+                  onPressedSure: () async {
+                    // String mountText = mountConrollers!.text;
+                    try {
+                      await context.read<ShippingCubit>().fechOrderShipping(
+                          ids ?? [],
+                          numberConrollers.text,
+                          images!,
+                          mountConrollers.text
+                          // mountText,
 
-                              // mountConroller?.text ?? ""
-                            );
-                        // String? cc = numberConrollers.text;
-                        Navigator.of(context).pop();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                                'تم شحن الطلب بنجاحا تااااااااااااااااااااااام'),
-                          ),
-                        );
-                        Navigator.of(context).pop();
-                        numberConrollers!.text = "";
-                        mountText = "";
-                        mountConrollers!.text = "";
-                        dateConrollers!.text = "";
-                        print(
-                            'amarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr ');
-                        // print('تبا لك $cc');
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('هنالك خطا ما  $e'),
-                          ),
-                        );
-                        print('ikjgtiorjijjguiwheiruthwuiertywuorie $images');
-                      }
-                    },
-                  );
-                });
-          }),
-    );
+                          // mountConroller?.text ?? ""
+                          );
+                      // String? cc = numberConrollers.text;
+                      Navigator.of(context).pop();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('تم شحن الطلب بنجاح '),
+                        ),
+                      );
+                      Navigator.of(context).pop();
+                      numberConrollers.clear();
+                      // mountText = "";
+                      mountConrollers.clear();
+                      dateConrollers.clear();
+                      print(
+                          'amarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr ');
+                      // print('تبا لك $cc');
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('هنالك خطا ما  $e'),
+                        ),
+                      );
+                      print('ikjgtiorjijjguiwheiruthwuiertywuorie $images');
+                    }
+                  },
+                );
+              });
+        });
   }
 }
